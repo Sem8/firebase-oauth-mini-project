93745f86551a837358633e0edb7f33fff936c146
Sem8 <mssemmi8@gmail.com>
2019-05-22 15:26:31 -0400

    server up and running

--- server/app.js
+++ server/app.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+    res.send(`Navigate to /api/auth to register to sign up and log in`)
+});
+
+module.exports = server;
