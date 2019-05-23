2a5fc0b549007b6acccc58b623311041095d937c
Sem8 <mssemmi8@gmail.com>
2019-05-22 20:20:55 -0400

    firebase setup on backend

--- server/data/helpers/users.js
+++ server/data/helpers/users.js
@@ -1,8 +1,25 @@
-const userdb = require('../data/dbConfig.js');
+const userdb = require("../dbConfig.js");
 
 module.exports = {
-    register,
-    findById
+  registerOrLogin,
+
 };
 
-async function 
+async function registerOrLogin(user) {
+  try {
+    const loggedInUser = await userdb("users")
+      .where({ firebase_id: user.firebase_id })
+      .first();
+    if (!!loggedInUser) {
+      return loggedInUser;
+    } else {
+      const [id] = await userdb("users").insert(user, "id");
+      const signedUpUser = await userdb("users")
+        .where({ id: id })
+        .first();
+      return signedUpUser;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,12 +1,28 @@
-const userRoute = require('express').Router();
-const userdb = require('../data/dbConfig.js');
+const userRoute = require("express").Router();
+const userdb = require("../data/dbConfig.js");
+const User = require("../data/helpers/users.js");
 
 // userRoute.get('/', (req, res) => {
 //     res.send('User route here');
 // });
 
-
-userRoute.post('/', (req, res) => {
-
-})
+userRoute.post("/", (req, res) => {
+  const { fullName, email, password, profilePhoto } = req.body;
+  const firebase_id = req.user;
+  User.registerOrLogin({
+    firebase_id,
+    fullName,
+    name,
+    email,
+    password,
+    profilePhoto
+  })
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(`User couldn't be authorized`);
+    });
+});
 module.exports = userRoute;
